7a6f920c85d61d6b2bd527143a528a0cc0d14118
saurabh <saurabhyadav42399@gmail.com>
2023-12-01 19:04:49 +0530

    updated return object of getFee

--- src/index.js
+++ src/index.js
@@ -526,11 +526,20 @@ class KeyringController extends EventEmitter {
     async getFees(velasTx, web3) {
         const { from, to, value, data, manualLimit } = velasTx
         const gasLimit = manualLimit ? manualLimit : await web3.eth.estimateGas({ to, from, value, data })
-        const gasPrice = await web3.eth.getGasPrice();
-        return { 
-            gasLimit:gasLimit,
-            gasPrice: parseInt(gasPrice)
-            }
+        const gasPrice = parseInt(await web3.eth.getGasPrice());
+        const fees = {
+            "slow":{
+                "gasPrice": parseInt(gasPrice)
+            },
+            "standard":{
+                "gasPrice": gasPrice + parseInt(gasPrice * 0.05)
+            },
+            "fast":{
+                "gasPrice": gasPrice + parseInt(gasPrice * 0.1)
+            },
+            baseFee: 0
+        }
+        return { gasLimit: gasLimit, fees: fees}
     }
 }
 
